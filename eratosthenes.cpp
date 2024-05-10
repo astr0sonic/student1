4c65ee039e6e7b92af541b11dea849fbf71b6d25
deluvoisss <dilyaranasvuzi@gmail.com>
2024-05-10 13:50:05 +0300

    task2

--- eratosthenes.cpp
+++ eratosthenes.cpp
@@ -4,23 +4,21 @@
 #include<cstring>
 std::vector<int> sieve(int n)
 {
+    std::vector<bool> prime(n + 1, true);
     std::vector<int> primes;
-    bool* prime;
-    prime = new bool[n + 1];
-    memset(prime, true, sizeof(prime));
-    for (int p = 2; p * p <= n; p++)
-    {
-        if (prime[p] == true)
-        {
+
+    for (int p = 2; p * p <= n; ++p) {
+        if (prime[p]) {
             for (int i = p * p; i <= n; i += p)
                 prime[i] = false;
         }
     }
-    for (int p = 2; p <= n; p++)
-    {
-        if (prime[p]==true) primes.push_back(p);
+
+    for (int p = 2; p <= n; ++p) {
+        if (prime[p])
+            primes.push_back(p);
     }
-    delete[]prime;
+
     return primes;
 
 }
