6f7928d11328846fb436dade9ca99952987bbd1e
deluvoisss <dilyaranasvuzi@gmail.com>
2024-05-14 00:51:57 +0300

    task3

---
+++ sundaram.cpp
@@ -0,0 +1,22 @@
+#include "sundaram.h"
+
+std::vector<int> sieve(int n) {
+    std::vector<int> primes;
+    int nNew = (n - 1) / 2;
+    bool* marked;
+    marked = new bool[nNew + 1];
+    memset(marked, false, sizeof(marked));
+    for (int i = 1; i <= nNew; i++)
+    {
+        for (int j = i; (i + j + 2 * i * j) <= nNew; j++)
+        {
+            marked[i + j + 2 * i * j] = true;
+        }
+    }
+    for (int i = 1; i <= nNew; i++)
+    {
+        if (marked[i] == false)
+            primes.push_back(2 * i + 1);
+    }
+    return primes;
+}
