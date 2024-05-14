c1afdeb8fd7711510384db62064b6b389d648e22
deluvoisss <dilyaranasvuzi@gmail.com>
2024-05-14 03:25:58 +0300

    task6

---
+++ rpn.cpp
@@ -0,0 +1,51 @@
+#include "rpn.h"
+#include<stack>
+double calculateRPN(const std::string& s)
+{
+	std::stack<double> itog;
+	double x;
+	for (int i = 0; i < s.length(); i++)
+	{
+		if (isdigit(s[i]))
+		{
+			x = s[i] - '0';
+			itog.push(x);
+		}
+		else if (s[i] == '/' || s[i] == '*' || s[i] == '+' || s[i] == '-')
+		{
+			x = itog.top();
+			itog.pop();
+			if (s[i] == '/')
+			{
+				
+				x = itog.top() / x;
+				
+				itog.pop();
+				itog.push(x);
+			}
+			else if (s[i] == '*')
+			{cout << x << " " << itog.top() << endl;
+				x = itog.top() * x;
+				
+				itog.pop();
+				itog.push(x);
+			}
+			else if (s[i] == '+')
+			{
+				cout << x << " " << itog.top() << endl;
+				x = itog.top() + x;
+				itog.pop();
+				itog.push(x);
+			}
+			else if (s[i] == '-')
+			{
+				
+				x = itog.top() - x;
+				
+				itog.pop();
+				itog.push(x);
+			}
+		}
+	}
+	return itog.top(
+}
