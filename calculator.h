0d52d552e12a039b3c840874ef4953e361f899af
deluvoisss <dilyaranasvuzi@gmail.com>
2024-05-14 13:03:12 +0300

    task7

--- calculator.h
+++ calculator.h
@@ -4,3 +4,4 @@ double calculate(const std::string& expr);
 bool isOperator(char c);
 int precedence(char op);
 double applyOperation(double a, double b, char op);
+bool isValidExpression(const std::string& expr);
